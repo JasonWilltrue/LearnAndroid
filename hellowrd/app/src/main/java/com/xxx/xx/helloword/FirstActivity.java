2d713d093369d2e9f06affd8ed0204e8fefdf4fb
148373644@qq.com <148373644@qq.com>
2018-08-29 23:43:09 +0800

    Intent在活动中使用，传递数据给上一页 下一页

--- hellowrd/app/src/main/java/com/xxx/xx/helloword/FirstActivity.java
+++ hellowrd/app/src/main/java/com/xxx/xx/helloword/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.xxx.xx.helloword;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,8 +16,8 @@ public class FirstActivity extends AppCompatActivity {
     //重写menu方法
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
-        return  true;
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
 
 
     }
@@ -22,22 +25,22 @@ public class FirstActivity extends AppCompatActivity {
     //菜单点击
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.add_item:
-                Toast.makeText(FirstActivity.this,"这个add",Toast.LENGTH_SHORT).show();
+                Toast.makeText(FirstActivity.this, "这个add", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.remove_item:
-                Toast.makeText(FirstActivity.this,"这个remove",Toast.LENGTH_LONG).show();
+                Toast.makeText(FirstActivity.this, "这个remove", Toast.LENGTH_LONG).show();
                 break;
         }
-        return  true;
+        return true;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-        Button button1 = (Button)findViewById(R.id.button_1);
+        Button button1 = (Button) findViewById(R.id.button_1);
 //        button1.setOnClickListener(new View.OnClickListener(){
 //            @Override
 //            public void onClick(View v) {
@@ -46,14 +49,97 @@ public class FirstActivity extends AppCompatActivity {
 //        });
 
         //销毁一个活动
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+
+//        Intent意图跳转页面   显示
+//         button1.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View v) {
+//                 Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                 startActivity(intent);
+//             }
+//         });
+
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent("com.xxx.xx.helloword.ACTION_START");
+//                startActivity(intent);
+//            }
+//        });
+
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+//                startActivity(intent);
+//            }
+//        });
+
+
+//        用意图 打开电话☎️功能
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("tel:10086"));
+//                startActivity(intent);
+//            }
+//        });
+
+
+//         传递给下一个页面
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String data = "hello second";
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                intent.putExtra("extra_data",data);
+//                startActivity(intent);
+//            }
+//        });
+
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+
+                startActivityForResult(intent, 1);
+
             }
         });
-
-
-
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode)
+        {
+            case 1 :
+                if(resultCode == RESULT_OK){
+                    String retrunData = data.getStringExtra("data_result");
+                    Log.d("first", "onActivityResult: "+retrunData);
+                }
+                break;
+                default:
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void onBackPressed () {
+        Intent intent = new Intent();
+        intent.putExtra("data_result", "first page");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+
 }
---
+++ hellowrd/app/src/main/java/com/xxx/xx/helloword/SecondActivity.java
@@ -0,0 +1,44 @@
+package com.xxx.xx.helloword;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.sql.BatchUpdateException;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d("SecondActivity", "传过来的数据: "+data);
+
+        Button button2 = (Button)findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent();
+               intent.putExtra("data_result","i am back first!😝");
+               setResult(RESULT_OK,intent);
+               finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+    }
+}
