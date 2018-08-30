5bd390e02d609f15d751809a7eb2f9c7289aa7ba
148373644@qq.com <148373644@qq.com>
2018-08-30 09:37:34 +0800

    💥🎆自定义引入布局🌈

--- hellowrd/app/src/main/java/com/xxx/xx/helloword/SecondActivity.java
+++ hellowrd/app/src/main/java/com/xxx/xx/helloword/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.xxx.xx.helloword;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +17,11 @@ public class SecondActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        //默认标题栏隐藏
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
 
         Intent intent = getIntent();
         String data = intent.getStringExtra("extra_data");
