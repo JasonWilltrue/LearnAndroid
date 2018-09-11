da3527cd436c78058c6bba0a5772585ac299511b
JasonWilltrue <148373644@qq.com>
2018-09-11 23:24:14 +0800

    更新生命周期 启动模式一种，回收—数据的保存与获取

--- ActivityLifeTest/app/src/main/java/com/example/jason/activitylifetest/MainActivity.java
+++ ActivityLifeTest/app/src/main/java/com/example/jason/activitylifetest/MainActivity.java
@@ -8,62 +8,64 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+
+    public static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
-        Button startNormalActivity = (Button)findViewById(R.id.start_normal_activity);
-        Button startDialogActivity = (Button)findViewById(R.id.start_dialog_activity);
+        setContentView(R.layout.activity_main);
+        Button startNormalActivity = (Button) findViewById(R.id.start_normal_activity);
+        Button startDialogActivity = (Button) findViewById(R.id.start_dialog_activity);
         startNormalActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,NormalActivity.class);
+                Intent intent = new Intent(MainActivity.this, NormalActivity.class);
                 startActivity(intent);
             }
         });
-
         startDialogActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,DialogActivity.class);
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
                 startActivity(intent);
             }
         });
     }
-    
+
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart");
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume");
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause");
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         Log.d(TAG, "onStop");
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
     }
+
     @Override
-    protected void onRestart(){
+    protected void onRestart() {
         super.onRestart();
         Log.d(TAG, "onRestart");
     }
--- ActivityLifeTest/app/src/main/java/com/example/jason/activitylifetest/NormalActivity.java
+++ ActivityLifeTest/app/src/main/java/com/example/jason/activitylifetest/NormalActivity.java
@@ -1,5 +1,6 @@
 package com.example.jason.activitylifetest;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
