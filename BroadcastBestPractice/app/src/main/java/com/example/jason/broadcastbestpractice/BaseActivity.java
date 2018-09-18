1044c5b4d4ae831ec2dad5dc0f108f7ff0896e25
JasonWilltrue <148373644@qq.com>
2018-09-18 13:44:24 +0800

    强制下线功能🙈

--- BroadcastBestPractice/app/src/main/java/com/example/jason/broadcastbestpractice/BaseActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/jason/broadcastbestpractice/BaseActivity.java
@@ -1,17 +1,69 @@
 package com.example.jason.broadcastbestpractice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+    private  ForceOffineReceiver receiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         ActivityCollector.addActivity(this);
     }
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.jason.broadcastbestpractice.FORCE_OFFINE");
+        receiver = new ForceOffineReceiver();
+        registerReceiver(receiver,intentFilter);
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(receiver != null){
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
     @Override
     protected void onDestroy(){
         super.onDestroy();
         ActivityCollector.removeActivity(this);
+    }
+
+    class ForceOffineReceiver extends BroadcastReceiver{
+        @Override
+        public void  onReceive(final Context context, Intent intent){
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("警告");
+            builder.setMessage("你的账号在别处登陆，本地强制下线");
+            builder.setCancelable(false);
+            builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll();  //销毁所以活动
+
+                    Intent intent = new Intent(context,LoginActivity.class);
+                    context.startActivity(intent);  //重启 Login
+                    
+
+
+                }
+            });
+            builder.show();
+        }
     }
 }
--- BroadcastBestPractice/app/src/main/java/com/example/jason/broadcastbestpractice/LoginActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/jason/broadcastbestpractice/LoginActivity.java
@@ -1,13 +1,44 @@
 package com.example.jason.broadcastbestpractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends BaseActivity {
+    private static final String TAG = "LoginActivity";
+    private EditText accountEdit;
+    private EditText passwordEdit;
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //获取id
+        accountEdit = (EditText) findViewById(R.id.account);
+        passwordEdit = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        //点击事件
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取账号与密码
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                Log.d(TAG, "account: "+account+"  password:  "+password);
+                //如果账号密码正确则跳转
+                if(account.equals("admin") && password.equals("123456")){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this,"账号或密码错误，请重新输入",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
